dec89a58c6a1282ab979678c0d9000b485f4d90f
thierry dothee <thierrydothee@protonmail.com>
2024-12-30 10:06:49 +0100

    project ready

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Faker;
+
+class UserFixtures extends Fixture
+{
+    
+    public function  __construct(private UserPasswordHasherInterface $passwordEncoder)
+    {
+        
+    }
+
+
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Faker\Factory::create('fr_FR');
+
+        for($usr = 0; $usr < 4; $usr++ ){
+            $user = new User();
+            $user->setEmail($faker->email())
+                ->setPassword($this->passwordEncoder->hashPassword($user,'password'))
+                ->setRoles(mt_rand(0,1) ===1 ? ['ROLE_USER']: ['ROLE_USER','ROLE_REDACTOR']);
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+        
+    }
+}
